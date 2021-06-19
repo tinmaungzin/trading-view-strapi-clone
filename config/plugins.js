c323d2a7b16d8543b127f357866449fe248943aa
root <root@DESKTOP-MRSLRT0.localdomain>
2021-06-19 14:31:47 +0630

    fixed plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -2,28 +2,25 @@ module exports = ({ env }) => {
 
 
     if(env('NODE_ENV') === 'production') {
-
-
          return {
-            upload: {   
+            upload: {
                 provider: 'aws-s3',
                 providerOptions: {
                     accessKeyId: env('AWS_ACCESS_KEY_ID'),
                     secretAccessKey: env('AWS_ACCESS_SECRET'),
                     region: env('AWS_REGION'),
                     params: {
-                        Bucket: env('AWS_BUCKET')
-                    }
-                }
-
-            }
-    }
+                        Bucket: env('AWS_BUCKET'),
+                    },
+                },
+            },
+        }
 
     }
 
 
    return {
-       
+
    }
     
 }
